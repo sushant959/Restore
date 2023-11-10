3f1869ad44d3becd196c2d890c961523c301af60
Sushant Kumar <sidsushant83@gmail.com>
2023-11-10 16:30:05 +0530

    End of section 5

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -2,7 +2,9 @@ import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, T
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
-import axios from "axios";
+import agent from "../../app/api/agent";
+import NotFound from "../../app/errors/NotFound";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 
 export default function ProductDetails() {
     const {id} = useParams<{id: string}>();
@@ -10,15 +12,15 @@ export default function ProductDetails() {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get(`http://localhost:5000/api/products/${id}`)
-        .then(response => setProduct(response.data))
+      id && agent.catalog.details(parseInt(id))
+        .then(response => setProduct(response))
         .catch(error => console.log(error))
         .finally(() => setLoading(false));
     },[id])
 
-    if(loading) return <h3>Loading...</h3>
+    if(loading) return <LoadingComponent message="Loading product..." />
 
-    if(!product) return <h3>Product not found</h3>
+    if(!product) return <NotFound />
 
     return (
         <Grid container spacing={6}>
